dd7f0ba8dc6968cc2449fb286750a64061936090
mavlyutov <mavlyutov@yandex-team.ru>
2014-10-17 19:46:35 +0400

    moved to jclouds 1.8.0

--- jclouds-plugin/src/test/java/jenkins/plugins/jclouds/compute/internal/TerminateNodesTest.java
+++ jclouds-plugin/src/test/java/jenkins/plugins/jclouds/compute/internal/TerminateNodesTest.java
@@ -1,17 +1,12 @@
 package jenkins.plugins.jclouds.compute.internal;
 
-import java.util.List;
-import java.util.concurrent.ExecutionException;
-
 import junit.framework.TestCase;
-
 import org.jclouds.ContextBuilder;
 import org.jclouds.compute.ComputeService;
 import org.jclouds.compute.ComputeServiceContext;
 import org.jclouds.compute.RunNodesException;
 import org.jclouds.compute.domain.NodeMetadata;
 import org.jclouds.logging.Logger;
-
 import shaded.com.google.common.base.Function;
 import shaded.com.google.common.base.Functions;
 import shaded.com.google.common.cache.CacheBuilder;
@@ -22,97 +17,100 @@ import shaded.com.google.common.collect.ImmutableMap;
 import shaded.com.google.common.collect.Iterables;
 import shaded.com.google.common.collect.Lists;
 
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
 public class TerminateNodesTest extends TestCase {
 
-	private ComputeService compute;
+    private ComputeService compute;
 
-	@Override
-	protected void setUp() throws Exception {
-		compute = ContextBuilder.newBuilder("stub").buildView(ComputeServiceContext.class).getComputeService();
-	}
+    @Override
+    protected void setUp() throws Exception {
+        compute = ContextBuilder.newBuilder("stub").buildView(ComputeServiceContext.class).getComputeService();
+    }
 
-	public void testSuspendOnlySuspendsNodesInQuestion() throws InterruptedException, ExecutionException, RunNodesException {
+    public void testSuspendOnlySuspendsNodesInQuestion() throws InterruptedException, ExecutionException, RunNodesException {
 
-		List<NodeMetadata> nodes = ImmutableList.copyOf(compute.createNodesInGroup("suspend", 10));
-		List<List<NodeMetadata>> split = Lists.partition(nodes, 5);
+        List<NodeMetadata> nodes = ImmutableList.copyOf(compute.createNodesInGroup("suspend", 10));
+        List<List<NodeMetadata>> split = Lists.partition(nodes, 5);
 
-		Iterable<RunningNode> runningNodesToSuspend = Iterables.transform(split.get(0), new Function<NodeMetadata, RunningNode>() {
+        Iterable<RunningNode> runningNodesToSuspend = Iterables.transform(split.get(0), new Function<NodeMetadata, RunningNode>() {
 
-			public RunningNode apply(NodeMetadata input) {
-				return new RunningNode("stub", "template", true, input);
-			}
+            public RunningNode apply(NodeMetadata input) {
+                return new RunningNode("stub", "template", true, input);
+            }
 
-		});
+        });
 
-		newTerminateNodes(compute).apply(runningNodesToSuspend);
+        newTerminateNodes(compute).apply(runningNodesToSuspend);
 
-		for (NodeMetadata node : split.get(0))
-			assertEquals(NodeMetadata.Status.SUSPENDED, compute.getNodeMetadata(node.getId()).getStatus());
-		for (NodeMetadata node : split.get(1))
-			assertEquals(NodeMetadata.Status.RUNNING, compute.getNodeMetadata(node.getId()).getStatus());
+        for (NodeMetadata node : split.get(0))
+            assertEquals(NodeMetadata.Status.SUSPENDED, compute.getNodeMetadata(node.getId()).getStatus());
+        for (NodeMetadata node : split.get(1))
+            assertEquals(NodeMetadata.Status.RUNNING, compute.getNodeMetadata(node.getId()).getStatus());
 
-	}
+    }
 
-	private TerminateNodes newTerminateNodes(ComputeService compute) {
-		LoadingCache<String, ComputeService> cache = CacheBuilder.newBuilder().build(
-				CacheLoader.<String, ComputeService> from(Functions.forMap(ImmutableMap.of("stub", compute))));
-		return new TerminateNodes(Logger.NULL, cache);
-	}
+    private TerminateNodes newTerminateNodes(ComputeService compute) {
+        LoadingCache<String, ComputeService> cache = CacheBuilder.newBuilder().build(
+                CacheLoader.<String, ComputeService>from(Functions.forMap(ImmutableMap.of("stub", compute))));
+        return new TerminateNodes(Logger.NULL, cache);
+    }
 
-	public void testDestroyOnlyDestroysNodesInQuestion() throws InterruptedException, ExecutionException, RunNodesException {
+    public void testDestroyOnlyDestroysNodesInQuestion() throws InterruptedException, ExecutionException, RunNodesException {
 
-		List<NodeMetadata> nodes = ImmutableList.copyOf(compute.createNodesInGroup("destroy", 10));
-		List<List<NodeMetadata>> split = Lists.partition(nodes, 5);
+        List<NodeMetadata> nodes = ImmutableList.copyOf(compute.createNodesInGroup("destroy", 10));
+        List<List<NodeMetadata>> split = Lists.partition(nodes, 5);
 
-		Iterable<RunningNode> runningNodesToDestroy = Iterables.transform(split.get(0), new Function<NodeMetadata, RunningNode>() {
+        Iterable<RunningNode> runningNodesToDestroy = Iterables.transform(split.get(0), new Function<NodeMetadata, RunningNode>() {
 
-			public RunningNode apply(NodeMetadata input) {
-				return new RunningNode("stub", "template", false, input);
-			}
+            public RunningNode apply(NodeMetadata input) {
+                return new RunningNode("stub", "template", false, input);
+            }
 
-		});
+        });
 
-		newTerminateNodes(compute).apply(runningNodesToDestroy);
+        newTerminateNodes(compute).apply(runningNodesToDestroy);
 
-		for (NodeMetadata node : split.get(0))
-			assertEquals(null, compute.getNodeMetadata(node.getId()));
-		for (NodeMetadata node : split.get(1))
-			assertEquals(NodeMetadata.Status.RUNNING, compute.getNodeMetadata(node.getId()).getStatus());
+        for (NodeMetadata node : split.get(0))
+            assertEquals(null, compute.getNodeMetadata(node.getId()));
+        for (NodeMetadata node : split.get(1))
+            assertEquals(NodeMetadata.Status.RUNNING, compute.getNodeMetadata(node.getId()).getStatus());
 
-	}
+    }
 
-	public void testSuspendAndDestroy() throws InterruptedException, ExecutionException, RunNodesException {
+    public void testSuspendAndDestroy() throws InterruptedException, ExecutionException, RunNodesException {
 
-		List<NodeMetadata> nodes = ImmutableList.copyOf(compute.createNodesInGroup("suspenddestroy", 10));
-		List<List<NodeMetadata>> split = Lists.partition(nodes, 5);
+        List<NodeMetadata> nodes = ImmutableList.copyOf(compute.createNodesInGroup("suspenddestroy", 10));
+        List<List<NodeMetadata>> split = Lists.partition(nodes, 5);
 
-		Iterable<RunningNode> runningNodesToSuspend = Iterables.transform(split.get(0), new Function<NodeMetadata, RunningNode>() {
+        Iterable<RunningNode> runningNodesToSuspend = Iterables.transform(split.get(0), new Function<NodeMetadata, RunningNode>() {
 
-			public RunningNode apply(NodeMetadata input) {
-				return new RunningNode("stub", "template", true, input);
-			}
+            public RunningNode apply(NodeMetadata input) {
+                return new RunningNode("stub", "template", true, input);
+            }
 
-		});
+        });
 
-		Iterable<RunningNode> runningNodesToDestroy = Iterables.transform(split.get(1), new Function<NodeMetadata, RunningNode>() {
+        Iterable<RunningNode> runningNodesToDestroy = Iterables.transform(split.get(1), new Function<NodeMetadata, RunningNode>() {
 
-			public RunningNode apply(NodeMetadata input) {
-				return new RunningNode("stub", "template", false, input);
-			}
+            public RunningNode apply(NodeMetadata input) {
+                return new RunningNode("stub", "template", false, input);
+            }
 
-		});
+        });
 
-		newTerminateNodes(compute).apply(Iterables.concat(runningNodesToSuspend, runningNodesToDestroy));
+        newTerminateNodes(compute).apply(Iterables.concat(runningNodesToSuspend, runningNodesToDestroy));
 
-		for (NodeMetadata node : split.get(0))
-			assertEquals(NodeMetadata.Status.SUSPENDED, compute.getNodeMetadata(node.getId()).getStatus());
-		for (NodeMetadata node : split.get(1))
-			assertEquals(null, compute.getNodeMetadata(node.getId()));
+        for (NodeMetadata node : split.get(0))
+            assertEquals(NodeMetadata.Status.SUSPENDED, compute.getNodeMetadata(node.getId()).getStatus());
+        for (NodeMetadata node : split.get(1))
+            assertEquals(null, compute.getNodeMetadata(node.getId()));
 
-	}
+    }
 
-	@Override
-	protected void tearDown() throws Exception {
-		compute.getContext().close();
-	}
+    @Override
+    protected void tearDown() throws Exception {
+        compute.getContext().close();
+    }
 }
